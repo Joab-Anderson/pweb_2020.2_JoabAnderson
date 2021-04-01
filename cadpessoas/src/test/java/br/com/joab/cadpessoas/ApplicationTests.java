4e7d60018f43cf27f104cb261ed9f44f45036b32
Joab-Anderson <joab.gomes@alunos.ufersa.edu.br>
2021-03-31 21:51:41 -0300

    crud u1 ex3

---
+++ cadpessoas/src/test/java/br/com/joab/cadpessoas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joab.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
