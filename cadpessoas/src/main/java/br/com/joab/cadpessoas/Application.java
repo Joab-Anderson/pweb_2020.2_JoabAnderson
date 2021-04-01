3eb10d7356bd1c33b5c8dbaaa2df7789d0df8d97
Joab-Anderson <joab.gomes@alunos.ufersa.edu.br>
2021-03-31 23:20:40 -0300

    v1.0

--- cadpessoas/src/main/java/br/com/joab/cadpessoas/Application.java
+++ cadpessoas/src/main/java/br/com/joab/cadpessoas/Application.java
@@ -2,10 +2,17 @@ package br.com.joab.cadpessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
+	@RequestMapping("/")
+	String home(){
+		return "Testando o Spring e Maven";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
