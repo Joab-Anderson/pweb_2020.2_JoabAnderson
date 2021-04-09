f0bf03b314d0c4484be72670c57602f8470b78c8
Joab-Anderson <joab.gomes@alunos.ufersa.edu.br>
2021-04-08 23:35:55 -0300

    v1.5.1
    Codigo secreto adicionado.

--- cadpessoas/src/main/resources/import.sql
+++ cadpessoas/src/main/resources/import.sql
@@ -3,4 +3,4 @@ INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('Maria Do Bai
 INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('Cristiano Ronaldo', 'cris7@yahhu.pt', parseDateTime('01/02/1987','dd/MM/yyyy'), 'Masculino');
 INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('Tarcisio Maia', 'tarsil@mil.br', parseDateTime('26/08/1916','dd/MM/yyyy'), 'Homem');
 INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('Lico Preto (vei do cuentro)', 'lico@ants.br', parseDateTime('05/11/1977','dd/MM/yyyy'), 'Não Informado');
-INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('Maria Joaquina', 'joaquina@rasis.com', parseDateTime('11/07/2000','dd/MM/yyyy'), 'Mulher');
+INSERT INTO pessoas (nome, email, data_nascimento, genero) VALUES ('A usurpadora', 'ausurpadora@rasis.com', parseDateTime('11/07/2000','dd/MM/yyyy'), 'Mulher');
